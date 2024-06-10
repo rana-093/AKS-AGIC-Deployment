e1d97e81bd443b7f88292451239d02aae8fc1c9d
RanaM <rana_093@yahoo.com>
2024-06-10 23:16:22 +0600

    Added updates apis

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+const STORAGE_ACCOUNT_NAME = 'blobformasud';
+const CONTAINER_NAME = 'filestorage';
+const BLOB_URL = `https://${STORAGE_ACCOUNT_NAME}.blob.core.windows.net/${CONTAINER_NAME}/`;
+
 app.get("/api", (req, res, next) => {
     res.json({"message": "Hello, World!"});
+});
+
+app.get('/pub/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const fileUrl = BLOB_URL + filename;
+    res.redirect(fileUrl);
 });
 
 app.listen(3000, () => {
